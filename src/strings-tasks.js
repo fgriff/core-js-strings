e80ce221d12c1af0945da2e42c833fca80b4d625
fgriff <anglirum@gmail.com>
2026-09-15 20:40:28 +0300

    feat: solve reverseString task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -257,8 +257,14 @@ function formatTime(minutes, seconds) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  let reversedStr = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reversedStr += str[i];
+  }
+
+  return reversedStr;
 }
 
 /**
