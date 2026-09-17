20fbab922b5ab3376538971e164aae355aa390bf
fgriff <anglirum@gmail.com>
2026-09-17 20:51:24 +0300

    feat: solve extractNameFromTemplate task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -470,8 +470,11 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const start = 'Hello, '.length;
+  const end = '!'.length;
+
+  return value.slice(start, -end);
 }
 
 /**
